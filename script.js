36514d8f5beaf9af35f67ea31408ad213847bd04
Norma Rivera <normarivera@Normas-iMac.lan>
2022-10-07 18:44:47 -0500

    resubmit

--- script.js
+++ script.js
@@ -1,27 +1,51 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var passwordLength = 8;
-var lowercase = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z";
-var uppercase = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,S,T,U,V,W,X,Y,Z";
-var numbers = "0,1,2,3,4,5,6,7,8,9,";
-var symbols = "!,@,#,$,%,&,*";
+
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQSTUVWXYZ";
+var numbers = "0123456789";
+var symbols = "!@#$%&*";
 
 function generatePassword() {
-window.prompt()
-for (i = 0; i < password.length; i++) {
-  var randomPassword = arr[Math.floor(Math.random() * password.length)];
-  document.write(randomPassword);
+  var passwordLength = window.prompt("how many characters")
+
+
+  if (passwordLength < 8 || passwordLength > 128) {
+    window.alert("Choose a password between 8 and 128");
+    return
+
+  }
+var asklowercase = confirm("do you want lowercase letters?")
+var askuppercase = confirm("do you want uppercase letters?")
+var asknumbers = confirm("do you want numbers letters?")
+var asksymbols = confirm("do you want symbols letters?")
+if (asklowercase===false && askuppercase===false && asknumbers===false && asksymbols===false) {
+  window.alert("choose at least one type of character");
+  return
+}
+var password = ""
+var options = ""
+if (asklowercase===true){
+  options += lowercase
+}
+if (askuppercase===true){
+  options += uppercase
+}
+if (asknumbers===true){
+  options += numbers
+}
+if (asksymbols===true){
+  options += symbols
 }
 
-if (passwordLength > 8 || passwordLength < 128) {
+  for (i = 0; i < passwordLength; i++) {
+    var randomPassword = options[Math.floor(Math.random() * options.length)];
+password += randomPassword
+  }
 
-  passwordText.value = password;
-}
+  return password
 
-
-return 
-
-// Add event listener to generate button
+  // Add event listener to generate button
 
 }
 
@@ -30,7 +54,7 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-passwordText.value = password;
+  passwordText.value = password;
 }
 
 
